2412f5550ac22e3887afa47f549c0cbed0aa626d
liusimin9527 <1246920026@qq.com>
2017-08-19 17:36:45 +0800

    createXML

--- AjaxUtil.js
+++ AjaxUtil.js
@@ -2,11 +2,11 @@ var AjaxUtil = {
   /**
    * ajax中的get请求
    * @param  {[type]}   url      [description]
-   * @param  {Function} callback [description]
+   * @param  {Function} callback [回调函数]
    * @return {[type]}            [description]
    */
   get: function (url, callback) {
-    var xhr = new XMLHttpRequest();
+    var xhr = createXHR();
 
     xhr.onreadystatechange = function () {
       if (xhr.status == 200 && xhr.readyState == 4) {
@@ -22,11 +22,11 @@ var AjaxUtil = {
    * ajax中的post请求
    * @param  {[type]}   url      [description]
    * @param  {[type]}   data     [description]
-   * @param  {Function} callback [description]
+   * @param  {Function} callback [回调函数]
    * @return {[type]}            [description]
    */
   post: function (url, data,callback) {
-    var xhr = new XMLHttpRequest();
+    var xhr = createXHR();
 
     xhr.onreadystatechange = function () {
       if (xhr.status == 200 && xhr.readyState == 4) {
@@ -37,5 +37,19 @@ var AjaxUtil = {
     xhr.open('post', url);
     xhr.setRequestHeader("Content-Type", "application/x-www--form-urlencoded");
     xhr.send(data);
-  }
 };
+/**
+  *  创建XMLHttpRequest对象
+  *  @return {[object]}   xhr      [description]
+  */
+function createXHR() {
+	var xhr = null;
+
+	if (window.XMLHttpRequest) {
+		xhr = new XMLHttpRequest();                    //其它浏览器
+	} else {
+		xhr = new ActiveXObject("Microsoft.XMLHTTP");  //IE5、IE6
+	}
+
+	return xhr;
+}
